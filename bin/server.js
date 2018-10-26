7b9ffd9e52b7c988018db353f01f22c2f3ba84b4
JP <sr.joaovirgili@gmail.com>
2018-10-25 23:55:16 -0300

    Adicionado repositorio responsaveis pelos metodos de crud

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@
 const app = require('../src/app');
 const http = require('http');
 const server = http.createServer(app);
-const PORT = 5000;
+const PORT = 3000;
 
 app.set('port', PORT);
 
--- src/controller/product-controller.js
+++ src/controller/product-controller.js
@@ -1,11 +1,11 @@
 'use strict'
 
 const Product = require('../models/product');
+const repository = require('../repositories/product-repo');
 
 exports.get = (req, res, next) => {
-    Product.find(
-        { active: true },
-        'title price slug')
+    repository
+        .get()
         .then((data) => {
             res.status(200).send(data);
         }).catch(e => {
@@ -14,9 +14,8 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-    Product.findOne(
-        { active: true, slug: req.params.slug },
-        'title description price slug tags')
+    repository
+        .getBySlug(req.params.slug)
         .then((data) => {
             if (data)
                 res.status(200).send(data);
@@ -28,7 +27,8 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-    Product.findById(req.params.id)
+    repository
+        .getById(req.params.id)
         .then((data) => {
             if (data)
                 res.status(200).send(data);
@@ -40,51 +40,42 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-    Product.find({
-        tags: req.params.tag,
-        active: true
-    }, 'title description price slug tags').then((data) => {
-        if (data)
-            res.status(200).send(data);
-        else
-            res.status(200).send({ message: "Produto não encontrado." });
-    }).catch(e => {
-        res.status(400).send(e);
-    });
+    repository
+        .getByTag(req.params.tag)
+        .then((data) => {
+            if (data)
+                res.status(200).send(data);
+            else
+                res.status(200).send({ message: "Produto não encontrado." });
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 };
 
 exports.post = (req, res, next) => {
-    var product = new Product(req.body);
-    product.save().then(() => {
-        res.status(201).send({ message: "Produto cadastrado com sucesso." });
-    }).catch(e => {
-        res.status(400).send({ message: "Falha ao cadastrar o produto.", data: e });
-    });
+    repository.create(req.body)
+        .then(() => {
+            res.status(201).send({ message: "Produto cadastrado com sucesso." });
+        }).catch(e => {
+            res.status(400).send({ message: "Falha ao cadastrar o produto.", data: e });
+        });
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    const updateJson = {
-        title: req.body.title,
-        description: req.body.description,
-        slug: req.body.slug,
-        price: req.body.price,
-    };
-
-    Product.findByIdAndUpdate(
-        id, { $set: updateJson }
-    ).then(() => {
-        res.status(200).send({ message: "Produto atualizado com sucesso." })
-    }).catch(() => {
-        res.status(400).send({ message: "Não foi possível realizar a atualização do produto." })
-    });
+    repository.update(req.params.id, req.body)
+        .then(() => {
+            res.status(200).send({ message: "Produto atualizado com sucesso." })
+        }).catch(() => {
+            res.status(400).send({ message: "Não foi possível realizar a atualização do produto." })
+        });
 };
 
 exports.delete = (req, res, next) => {
-    const id = req.body.id;
-    Product.findByIdAndDelete(id).then(() => {
-        res.status(200).send({ message: "Produto deletado com sucesso." })
-    }).catch(() => {
-        res.status(400).send({ message: "Não foi possível deletar o produto." })
-    });
+    repository
+        .delete(req.body.id)
+        .then(() => {
+            res.status(200).send({ message: "Produto deletado com sucesso." })
+        }).catch(() => {
+            res.status(400).send({ message: "Não foi possível deletar o produto." })
+        });
 };
