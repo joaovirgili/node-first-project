fdfb744998e4823aaf1c6241b0749621d8cf21a5
JP <sr.joaovirgili@gmail.com>
2018-10-25 22:33:29 -0300

    ajustando a formatação do arquivo

--- src/app.js
+++ src/app.js
@@ -3,10 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-
-=======
->>>>>>> bc6707dfcc2c02a804ed6a2ddf3dcf87192396b6
 
 // Carrega as rotas
 const index = require('./routes/indexRoute');
@@ -23,10 +19,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', index);
 app.use('/products', product);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> bc6707dfcc2c02a804ed6a2ddf3dcf87192396b6
 module.exports = app;
 
 
