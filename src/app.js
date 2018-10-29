c742c04715dd6c27b2beaea13923e2f0d82196bb
JP <sr.joaovirgili@gmail.com>
2018-10-29 01:58:35 -0300

    Começando a implementar a view

--- src/app.js
+++ src/app.js
@@ -16,8 +16,20 @@ const app = express();
 // Conecta ao banco
 mongoose.connect(config.connectionString);
 
+app.set('views', __dirname + '/views');
+app.use(express.static( __dirname + '/views'))
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', index);
 app.use('/products', product);
--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -4,10 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1",
-    })
+    res.render("index.ejs");
 });
 
 module.exports = router;
